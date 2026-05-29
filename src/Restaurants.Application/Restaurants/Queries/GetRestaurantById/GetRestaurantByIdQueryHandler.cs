e211547167b01584e391a29e24609ca0292ad678
bhargav koya <bhargavkoya99@gmail.com>
2026-05-29 15:28:17 +0100

    Logo Url added

--- src/Restaurants.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQueryHandler.cs
+++ src/Restaurants.Application/Restaurants/Queries/GetRestaurantById/GetRestaurantByIdQueryHandler.cs
@@ -2,7 +2,9 @@
 using MediatR;
 using Microsoft.Extensions.Logging;
 using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
 using Restaurants.Domain.Exceptions;
+using Restaurants.Domain.Interfaces;
 using Restaurants.Domain.Repositories;
 using System;
 using System.Collections.Generic;
@@ -12,17 +14,22 @@ using System.Threading.Tasks;
 
 namespace Restaurants.Application.Restaurants.Queries.GetRestaurantById
 {
-    public class GetRestaurantByIdQueryHandler(ILogger<GetRestaurantByIdQueryHandler> logger,IMapper mapper,IRestaurantsRepository restaurantsRepository) : IRequestHandler<GetRestaurantByIdQuery, RestaurantDto>
+    public class GetRestaurantByIdQueryHandler(ILogger<GetRestaurantByIdQueryHandler> logger,
+    IRestaurantsRepository restaurantsRepository,
+    IMapper mapper,
+    IBlobStorageService blobStorageService) : IRequestHandler<GetRestaurantByIdQuery, RestaurantDto>
     {
         public async Task<RestaurantDto> Handle(GetRestaurantByIdQuery request, CancellationToken cancellationToken)
         {
-            var restaurant = await restaurantsRepository.GetByIdAsync(request.Id);
-            if (restaurant == null)
-            {
-                logger.LogWarning("Restaurant with ID {RestaurantId} not found.", request.Id);
-                throw new NotFoundException(nameof(request), request.Id.ToString());
-            }
-            return mapper.Map<RestaurantDto>(restaurant);
+            logger.LogInformation("Getting restaurant {RestaurantId}", request.Id);
+            var restaurant = await restaurantsRepository.GetByIdAsync(request.Id)
+                    ?? throw new NotFoundException(nameof(Restaurant), request.Id.ToString());
+
+            var restaurantDto = mapper.Map<RestaurantDto>(restaurant);
+
+            restaurantDto.LogoSasUrl = blobStorageService.GetBlobSasUrl(restaurant.LogoUrl);
+
+            return restaurantDto;
         }
     }
 }
